9ead36968ba9ff96ad7aa591e6119ca06b785e81
julien <Julien Kermes>
2016-02-07 20:01:48 +0100

    Multithreaded C Server working fine

--- server_c/server.c
+++ server_c/server.c
@@ -1,58 +1,90 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <sys/socket.h>
 #include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
 #include "utils.h"
 
-int main(int argc, char *argv[])
-{
-    struct sockaddr_in my_addr, peer_addr;
-    int sock, error, peer_addr_len, scom, i, j;
-    char host[1024], serv[1024], inBuffer[1024], outBuffer[1024];
-    
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) != -1) {       
-        my_addr.sin_family = AF_INET;
-        my_addr.sin_port = htons(9999);
-        my_addr.sin_addr.s_addr = INADDR_ANY;
-        
-        if (bind(sock, (struct sockaddr *) &my_addr, sizeof(struct sockaddr_in)) != 0) {
-			printf("Error during port binding\n");
-			exit(1);
-		}
+void *connection_handler(void *);
 
-        if (listen(sock, 10) != 0) {
-			printf("Error during port listening\n");
-			exit(1);
-		}
-		
-		printf("Listening on port %d\n", ntohs(my_addr.sin_port));
-		
-		while (1) {
-			
-			peer_addr_len = sizeof(struct sockaddr_in);
-			scom = accept(sock, (struct sockaddr *)&peer_addr, &peer_addr_len);
-			getnameinfo((struct sockaddr *)&peer_addr, sizeof(peer_addr), host, sizeof(host), serv, sizeof(serv), 0);
-			printf("Received from %s on port %s\n", host, serv);
-			
-			while (1) {
-				recv(scom, inBuffer, sizeof(inBuffer), 0);
-	       		printf("\nReceived data: %s\n", inBuffer);
-	            memset(outBuffer, 0, sizeof(outBuffer));
-	            
-	            printf("Vowels: %d\n", countVowel(inBuffer));
-	            printf("Consonants: %d\n", countConsonants(inBuffer));
-	       
-	            //send(scom, outBuffer, strlen(outBuffer), 0);
-	            memset(inBuffer, 0, sizeof(inBuffer));
-	            //if (strcmp(inBuffer, "END") == 0) {
-	            //    break; 
-			    //}
-            }
-		};
-		
-		close(scom);        
-        close(sock);
-    } 
+int main(int argc , char *argv[])
+{
+    int socket_desc , new_socket , c , *new_sock;
+    struct sockaddr_in server , client;
+    char *message;
+
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1) {
+        printf("Could not create socket");
+    }
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 9999 );
+
+    if (bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0) {
+        puts("bind failed");
+        return 1;
+    }
+    puts("bind done");
+
+    listen(socket_desc , 3);
+
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+    while ((new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c))) {
+        puts("Connection accepted");
+
+        message = "Hello Client , I have received your connection. And now I will assign a handler for you\n";
+        write(new_socket , message , strlen(message));
+
+        pthread_t sniffer_thread;
+        new_sock = malloc(1);
+        *new_sock = new_socket;
+
+        if (pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0) {
+            perror("could not create thread");
+            return 1;
+        }
+        puts("Handler assigned");
+    }
+
+    if (new_socket<0) {
+        perror("accept failed");
+        return 1;
+    }
+
+    return 0;
+}
+
+void *connection_handler(void *socket_desc)
+{
+    int sock = *(int*)socket_desc;
+    int read_size;
+    char *message , client_message[2000];
+
+    message = "Greetings! I am your connection handler\n";
+    write(sock , message , strlen(message));
+
+    message = "Now type something and i shall repeat what you type \n";
+    write(sock , message , strlen(message));
+
+    while ((read_size = recv(sock , client_message , 2000 , 0)) > 0 ) {
+        printf("Vowels: %d\n", countVowel(client_message));
+        printf("Consonants: %d\n", countConsonants(client_message));
+    }
+
+    if (read_size == 0) {
+        puts("Client disconnected");
+        fflush(stdout);
+    }
+    else if(read_size == -1) {
+        perror("recv failed");
+    }
+
+    free(socket_desc);
+
+    return 0;
 }
